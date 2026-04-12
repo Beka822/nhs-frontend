a6786ebb2321fbea59c705a168731e2e68638861
Beka Eli <bekaeli27@gmail.com>
2026-04-12 18:12:42 +0300

    importing api

--- src/Pages/Hospitals/CreateHospital.jsx
+++ src/Pages/Hospitals/CreateHospital.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import axios from "axios";
+import api from "../../api/axios";
 export default function CreateHospital() {
     const navigate=useNavigate()
     const [hospitalId,setHospitalId]=useState("")
