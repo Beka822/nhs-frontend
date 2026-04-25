74acfc2822628a1c0b7209a0c3cecc872cb8ab95
Beka Eli <bekaeli27@gmail.com>
2026-04-25 17:36:45 +0300

    adding bracket

--- src/Visits/VisitDetail.jsx
+++ src/Visits/VisitDetail.jsx
@@ -13,9 +13,9 @@ const VisitDetail=()=>{
     useEffect(()=>{
         const fetchVisit=async ()=>{
             try{ 
-        const visitRes=await api.get(`/visits/${patient_id}/${visit_id}`,
+        const visitRes=await api.get(`/visits/${patient_id}/${visit_id}`,{
             headers:{Authorization: `Bearer ${token}`}
-        );
+            });
             setVisit(visitRes.data);
             const patientId=visitRes.data.patient_id;
             //once visit loads, fetch addendum using patient_id + visit_id
