9f2b7df174573b9d38f5f20e1fdfd4dff80cda3b
Beka Eli <bekaeli27@gmail.com>
2026-05-07 10:30:29 +0300

    visit creation

--- src/Visits/CreateVisit.jsx
+++ src/Visits/CreateVisit.jsx
@@ -161,9 +161,11 @@ const CreateVisit=()=>{
                 Authorization: `Bearer ${token}`
             }
         });
-        setShowPaymentModal(false);
+        alert("Visit created successfully");
         navigate(`/patients/${patient_id}`);
-    };
+       {/* setShowPaymentModal(false);
+        navigate(`/patients/${patient_id}`);
+    */}};
     return (
         <div className="p-6 max-w-3xl mx-auto">
             <h1 className="text-2xl font-bold mb-6">
